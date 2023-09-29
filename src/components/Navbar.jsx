109d89c6c922f2ca15083a74383b4ac7170da56a
aggie-l <142058426+aggie-l@users.noreply.github.com>
2023-09-29 19:05:13 +0100

    Add files via upload

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import '../App.css'
+
+function Navbar() {
+
+    return (
+      <nav>
+          <img src='../public/assets/airbnb.png' className='nav--logo' />
+      </nav>
+    )
+  }
+  
+  export default Navbar
